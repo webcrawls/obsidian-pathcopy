c6d3b7d57376d37e53ff2f09bf2c3feadf9792f4
webcrawls <github.com@kaden.sh>
2025-04-05 16:34:44 -0400

    🎨 add copy icon to ctx menu item

--- main.ts
+++ main.ts
@@ -11,11 +11,11 @@ export default class PathCopyPlugin extends Plugin {
 		this.registerEvent(this.app.workspace.on('file-menu', (menu, file, src, leaf) => {
 			menu.addItem(item => {
 				item.setTitle('Copy as Path')
+					.setIcon('copy')
 					.onClick(() => {
 						const vaultPath = file.vault.adapter.basePath
 						const relativeFilePath = file.path
 						const resolvedPath = path.resolve(vaultPath, relativeFilePath)
-						// console.log({vaultPath, relativeFilePath, resolvedPath})
 
 						navigator.clipboard.writeText(resolvedPath)
 					})
